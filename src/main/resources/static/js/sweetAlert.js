30a6d410d1f95e42f3b6b531e2119ade778556f6
*Andrea <karen.guarnizo.mt@correo.usa.edu.co>
2021-11-02 17:10:35 -0500

    actualizo front net

---
+++ src/main/resources/static/js/sweetAlert.js
@@ -0,0 +1,20 @@
+Swal.fire({
+    title: "Bienvenido!",
+    text:"Espero te guste mi pagina",
+    icon: 'success',
+    width:'50%',
+    background : '#4C62A8',
+    backdrop: true,
+    timer: 2200,
+    timmerProgressBar: true,
+    allowOutsideClick:false,
+    showCloseButton: true,
+    closeButtonAriaLabel: 'Cerrar alerta'
+    
+
+});
+
+
+
+
+
